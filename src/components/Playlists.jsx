4f9e1724fb2bda6ad8d13ca5fde3aa4c402299eb
Ritish Srivastava <55885901+ritish1912@users.noreply.github.com>
2022-07-20 18:03:37 +0530

    src

---
+++ src/components/Playlists.jsx
@@ -0,0 +1,78 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { reducerCases } from "../utils/Constants";
+import { useStateProvider } from "../utils/StateProvider";
+
+export default function Playlists() {
+  const [{ token, playlists }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const getPlaylistData = async () => {
+      const response = await axios.get(
+        "https://api.spotify.com/v1/users/vhdm80af7f11vqpbke6sfhw83/playlists?offset=0&limit=20",
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const { items } = response.data;
+      console.log(response);
+      const playlists = items.map(({ name, id }) => {
+        return { name, id };
+      });
+      dispatch({ type: reducerCases.SET_PLAYLISTS, playlists });
+    };
+
+    getPlaylistData();
+  }, [token, dispatch]);
+  const changeCurrentPlaylist = (selectedPlaylistId) => {
+    dispatch({ type: reducerCases.SET_PLAYLIST_ID, selectedPlaylistId });
+  };
+  return (
+    <Container>
+      <ul>
+        {playlists.map(({ name, id }) => {
+         console.log(name);
+          return (
+            <li key={id} onClick={() => changeCurrentPlaylist(id)}>
+              {name}
+            </li>
+            
+          );
+        })}
+      </ul>
+      
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  color: #b3b3b3;
+  height: 100%;
+  overflow: hidden;
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    height: 55vh;
+    max-height: 100%;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.7rem;
+      &-thumb {
+        background-color: rgba(255, 255, 255, 0.6);
+      }
+    }
+    li {
+      transition: 0.3s ease-in-out;
+      cursor: pointer;
+      &:hover {
+        color: white;
+      }
+    }
+  }
+`;
